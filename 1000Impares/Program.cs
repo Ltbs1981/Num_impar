87ed1642e33b1bea87635d37ccb81cdfb9b6d04c
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-08 17:17:30 -0300

    Projeto inicial

---
+++ 1000Impares/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1000Impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fala bonitão!");
+            Console.WriteLine("Digite um número entre 1 e 1000");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero >= 1 && numero <= 1000)
+            {
+                Console.WriteLine($"Impares até {numero}");
+                for (int i = 1; i <= numero; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("O número tem que estar entre 1 e 1000");
+            }
+        }
+    }
+}
